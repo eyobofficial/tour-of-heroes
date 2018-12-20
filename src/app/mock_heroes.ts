280bebc64c89f7b5f3d3269421aadb2943ad64be
eyob tariku <eyobtariku@gmail.com>
2018-12-20 17:49:13 +0300

    Create list and detail views for all heroes
    * Concludes Part-2 (`Displaying a List`) of the tutorial.

---
+++ src/app/mock_heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {
+    id: 1,
+    name: 'Windstorm'
+  },
+  {
+    id: 2,
+    name: 'Batman'
+  },
+  {
+    id: 3,
+    name: 'Superman'
+  },
+  {
+    id: 4,
+    name: 'Ironman'
+  },
+  {
+    id: 5,
+    name: 'Hulk'
+  }
+];
